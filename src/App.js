a1ab9446fbba8224125a5fe4821f4b0ab698126d
Avi Teller <ateller@kaleidovision.co.uk>
2019-08-30 15:56:16 +0100

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
+import React from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
 function App() {
   return (
     <div>
-      <Palette {...seedColors[8]}/>
-      
+      <Palette {...seedColors[8]} />
     </div>
   );
 }
