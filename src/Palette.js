eb9d272560ff77d124c926ad89aee5f336cfeb61
Avi Teller <ateller@kaleidovision.co.uk>
2019-08-30 15:22:01 +0100

    first

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Palette</h1>
+      </div>
+    )
+  }
+}
+
+export default Palette
