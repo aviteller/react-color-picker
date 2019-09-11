69ff99d51e6b314a230334a15cf72af5c122d49b
Avi Teller <ateller@kaleidovision.co.uk>
2019-09-11 16:35:09 +0100

    refaot colorbox styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorhelpers";
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+export class App extends Component {
+  findPalette = id => {
+    return seedColors.find(palette => {
+      return palette.id === id;
+    });
+  };
+
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+        <Route path="/palette/:paletteId/:colorId"   render={routeProps => (
+            <SingleColorPalette
+            colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )} />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[1])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -1,39 +1,43 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 import "./Palette.css";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
 
 export class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel = level => {
     this.setState({ level });
   };
+
+  changeFormat = val => {
+    this.setState({ format: val });
+  };
+
   render() {
-    const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { colors, paletteName, emoji, id } = this.props.palette;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} id={color.id} paletteId={id} showingFullPalette={true}/>
     ));
-    
+
     return (
       <div className="Palette">
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            onAfterChange={this.changeLevel}
-            step={100}
-          />
-        </div>
-        {/* Navbar herer */}
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+          showingAllColors
+        />
+
         <div className="Palette-colors">{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
